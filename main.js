b73360dc545f42317e71fe0549703661be51eb2d
Alan Killian <akillian@outlook.com>
2023-01-06 18:39:59 -0500

    commit

--- main.js
+++ main.js
@@ -6,21 +6,24 @@ const store = new SteinStore(
     'https://api.steinhq.com/v1/storages/63b713daeced9b09e9b0a04f'
 );
 
-const el = document.getElementById('signup');
+const signUp = document.getElementById('signup');
 const user = document.querySelector('.user');
 const button = document.querySelector('.btn');
 const form = document.querySelector('form');
 const crowdImg = document.querySelector("img[src='./img/crowd.webp']");
+const attendees = document.querySelector('.attendees');
 
-// Add event listener's
-el.addEventListener('click', () => {
+// ========= Event listener's =============== //
+
+// Sign-up button listener
+signUp.addEventListener('click', () => {
     user.classList.toggle('form--appear');
     crowdImg.classList.toggle('visible');
 });
 
 // Event listener to submit form data to Google sheets
 button.addEventListener('click', function () {
-    // Vars
+    // lets
     let formData = new FormData(form);
     let arrayFormData = [];
 
@@ -35,7 +38,26 @@ button.addEventListener('click', function () {
     });
 });
 
+// Event listener to retrieve attendees & create template for
+// attendee table
+attendees.addEventListener('click', () => {
+    store.read('signup').then((data) => {
+        console.log(data);
+    });
 
-/* store.read('signup').then((data) => {
-    console.log(data);
-}); */
+    // Handlebars template code
+    let template = document.getElementById('template').innerHTML;
+
+    //Compile the template
+    let compiled_template = Handlebars.compile(template);
+
+    //Render the data into the template
+    let rendered = compiled_template({ name: 'Luke', power: 'force' });
+
+    //Overwrite the contents of #target with the renderer HTML
+    document.getElementById('target').innerHTML = rendered;
+
+    user.classList.toggle('visible');
+
+    //  ============= End Template code =============== //
+});
