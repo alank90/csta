617442e9512e2628b8a1e4e24848bf9b7912ce1f
Alan K <akillian@outlook.com>
2023-01-06 14:28:12 -0500

    Added stein append ability.

--- main.js
+++ main.js
@@ -6,18 +6,36 @@ const store = new SteinStore(
     'https://api.steinhq.com/v1/storages/63b713daeced9b09e9b0a04f'
 );
 
-store
-    .append('signup', [
-        {
-            student_name: 'Alan Killian',
-            teacher_name: 'Greg Leong',
-            group: 'Comp Sci II',
-        },
-    ])
-    .then((res) => {
-        console.log(res);
-    });
+const el = document.getElementById('signup');
+const user = document.querySelector('.user');
+const button = document.querySelector('.btn');
+const form = document.querySelector('form');
+const crowdImg = document.querySelector("img[src='./img/crowd.webp']");
 
-store.read('signup').then((data) => {
-    console.log(data);
+// Add event listener's
+el.addEventListener('click', () => {
+    user.classList.toggle('form--appear');
+    crowdImg.classList.toggle('visible');
 });
+
+// Event listener to submit form data to Google sheets
+button.addEventListener('click', function () {
+    // Vars
+    let formData = new FormData(form);
+    let arrayFormData = [];
+
+    form.classList.add('form--no');
+    // Need to convert FormData object to array object for stein to accept.
+    formData = Object.fromEntries(formData.entries());
+    arrayFormData.push(formData);
+
+    store.append('signup', arrayFormData).then((res) => {
+        console.log(res);
+        form.reset();
+    });
+});
+
+
+/* store.read('signup').then((data) => {
+    console.log(data);
+}); */
