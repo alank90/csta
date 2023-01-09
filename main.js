e77a3ffc4119a5e7f1d48f0de6787d6b719a8b77
Alan K <akillian@outlook.com>
2023-01-09 14:30:38 -0500

    Add .env variable

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 import './css/style.css';
 import SteinStore from 'stein-js-client';
 
-// Create SStein Store instance
-const store = new SteinStore(
-    'https://api.steinhq.com/v1/storages/63b713daeced9b09e9b0a04f'
-);
+// Create Stein Store instance
+const VITE_STEIN_URL = import.meta.env.VITE_STEIN_URL;
+const store = new SteinStore(VITE_STEIN_URL);
 
 const signUp = document.getElementById('signup');
 const user = document.querySelector('.user');
