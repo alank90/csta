991a35bea244aa4d768118dfb5a9b1bd689fe83f
Alan K <akillian@outlook.com>
2023-01-17 14:25:49 -0500

    Commit. Start Auth0 addtion.

--- main.js
+++ main.js
@@ -139,3 +139,60 @@ tableTemplate.addEventListener('click', (e) => {
         tableTemplate.classList.toggle('visible');
     }
 });
+
+// ------------------------------------------------------------------------ //
+// ==================================================================== //
+// ============== Auth0 Client JS ===================================== //
+// ==================================================================== //
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientID = import.meta.env.VITE_CLIENT_ID;
+auth0
+    .createAuth0Client({
+        domain: domain,
+        clientId: clientID,
+        authorizationParams: {
+            redirect_uri: window.location.origin,
+        },
+    })
+    .then(async (auth0Client) => {
+        // Assumes a button with id "login" in the DOM
+        const loginButton = document.getElementById('login');
+
+        loginButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            auth0Client.loginWithRedirect();
+        });
+
+        if (
+            location.search.includes('state=') &&
+            (location.search.includes('code=') ||
+                location.search.includes('error='))
+        ) {
+            await auth0Client.handleRedirectCallback();
+            window.history.replaceState({}, document.title, '/');
+        }
+
+        // Assumes a button with id "logout" in the DOM
+        const logoutButton = document.getElementById('logout');
+
+        logoutButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            auth0Client.logout();
+        });
+
+        const isAuthenticated = await auth0Client.isAuthenticated();
+        const userProfile = await auth0Client.getUser();
+
+        // Assumes an element with id "profile" in the DOM
+        const profileElement = document.getElementById('profile');
+
+        if (isAuthenticated) {
+            profileElement.style.display = 'block';
+            profileElement.innerHTML = `
+              <p>${userProfile.name}</p>
+              <img src="${userProfile.picture}" />
+            `;
+        } else {
+            profileElement.style.display = 'none';
+        }
+    });
