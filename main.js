d281c0a16bfe54da8d4652d76f6f325f45a9158a
Alan K <akillian@outlook.com>
2023-01-25 11:55:05 -0500

    Added form validty thru reportValidity().

--- main.js
+++ main.js
@@ -27,54 +27,69 @@ signUp.addEventListener('click', () => {
 // ================================================================== //
 
 button.addEventListener('click', function () {
-    // lets grab data from form
-    let formData = new FormData(form);
-    let arrayFormData = [];
+    const isValid = form.reportValidity();
+    // Run form validity check
+    form.reportValidity();
 
-    form.classList.add('form--no');
+    if (isValid) {
+        // Form passed validity check, lets grab data from form
+        let formData = new FormData(form);
+        let arrayFormData = [];
+        form.classList.add('form--no');
 
-    // Need to convert FormData object to array object for stein to accept.
-    formData = Object.fromEntries(formData.entries());
-    arrayFormData.push(formData);
+        // Need to convert FormData object to array object for stein to accept.
+        formData = Object.fromEntries(formData.entries());
+        arrayFormData.push(formData);
 
-    // Build an array of objects to send to stein as group
-    let teamMember = {};
-    let teamMembers = [];
+        // Build an array of objects to send to stein as group
+        let teamMember = {};
+        let teamMembers = [];
 
-    // Iterate thru the array and check if any group member
-    // names are null so we dont send them to Google sheets
-    for (let i = 1; i < 5; i++) {
-        if (formData['student_name' + i]) {
-            teamMember['team_name'] = formData['team_name'];
-            teamMember['programming_language'] =
-                formData['programming_language'];
-            teamMember['school'] = formData['school'];
-            teamMember['teacher_advisor'] = formData['teacher_advisor'];
-            teamMember['faculty_email'] = formData['faculty_email'];
-            teamMember['faculty_shirt_size'] = formData['faculty_shirt_size'];
-            teamMember['student_name'] = formData['student_name' + i];
-            teamMember['student_email'] = formData['student_email' + i];
-            teamMember['shirt_size'] = formData['shirt_size' + i];
-            teamMember['grade'] = formData['grade' + i];
+        // Iterate thru the array and check if any group member
+        // names are null so we dont send them to Google sheets
+        for (let i = 1; i < 5; i++) {
+            if (formData['student_name' + i]) {
+                teamMember['team_name'] = formData['team_name'];
+                teamMember['programming_language'] =
+                    formData['programming_language'];
+                teamMember['school'] = formData['school'];
+                teamMember['teacher_advisor'] = formData['teacher_advisor'];
+                teamMember['faculty_email'] = formData['faculty_email'];
+                teamMember['faculty_shirt_size'] =
+                    formData['faculty_shirt_size'];
+                teamMember['student_name'] = formData['student_name' + i];
+                teamMember['student_email'] = formData['student_email' + i];
+                teamMember['shirt_size'] = formData['shirt_size' + i];
+                teamMember['grade'] = formData['grade' + i];
 
-            // Push students in group onto an array
-            teamMembers = [...teamMembers, teamMember];
+                // Push students in group onto an array
+                teamMembers = [...teamMembers, teamMember];
 
-            // clear the teammember object to be reused
-            teamMember = {};
+                // clear the teammember object to be reused
+                teamMember = {};
+            }
         }
-    }
 
-    // Let's hit the stein API with the form data in array form
-    try {
-        store.append('signup', teamMembers).then((res) => {
-            console.log(res);
-            form.reset();
-            button.innerHTML = `Sign-up succesful. ${res.updatedRange}`;
-        });
-    } catch (error) {
-        console.error(error);
-        alert('Add attendee failed! Sorry try again.');
+        // Let's hit the stein API with the form data in array form
+        try {
+            store.append('signup', teamMembers).then((res) => {
+                console.log(res);
+                form.reset();
+                button.innerHTML = `Sign-up succesful. ${res.updatedRange}`;
+                // Delete error message if there
+                if (document.querySelector('.error-message').innerHTML !== '') {
+                    document.querySelector('.error-message').innerHTML = '';
+                }
+            });
+        } catch (error) {
+            console.error(error);
+            alert('Add attendee failed! Sorry try again.');
+        }
+    } else {
+        // Form failed validity check.
+        document.querySelector(
+            '.error-message'
+        ).innerHTML = `Form incorrect. Please check.`;
     }
 });
 
